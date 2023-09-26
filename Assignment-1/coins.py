fb57a615e2fbb16b40cb0744484aa5cef90e068c
Onkar <onkarrai06@gmail.com>
2023-09-26 20:17:44 +0500

    coins: Changed the code a bit

--- Assignment-1/coins.py
+++ Assignment-1/coins.py
@@ -1,9 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("images/coins.png",1)
-
-img = cv2.convertScaleAbs(img, 1, 0.5)
+img = cv2.imread("Assignment-1/images/coins.png",1)
 
 #original
 cv2.imshow('original image',img)
@@ -12,9 +10,13 @@ cv2.imshow('original image',img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #binary scale
-thresh, binary = cv2.threshold(gray,71,255, cv2.THRESH_BINARY)
+thresh, binary = cv2.threshold(gray,140,255, cv2.THRESH_BINARY)
 binary = cv2.bitwise_not(binary)
-cv2.imshow('binary image', binary)
+
+result_image = cv2.bitwise_and(img, img, mask=binary)
+
+cv2.imshow('Final segmented Image', result_image)
+cv2.imshow("Binary Image", binary)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
