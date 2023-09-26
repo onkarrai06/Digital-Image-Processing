5800b8c7e26476b4da0821e2c96bcd787a7ed8b7
Syed Arsal Rahman <arsalazfar@hotmail.com>
2023-09-26 23:18:46 +0500

    added count text to image

--- Assignment-1/coins.py
+++ Assignment-1/coins.py
@@ -11,14 +11,19 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #binary scale
 thresh, binary = cv2.threshold(gray,140,255, cv2.THRESH_BINARY)
-binary = cv2.bitwise_not(binary)
 
+binary = cv2.bitwise_not(binary)
 result_image = cv2.bitwise_and(img, img, mask=binary)
 
-cv2.imshow('Final segmented Image', result_image)
+#counting objects
+count, _ = cv2.connectedComponents(binary)
+
+
+result_image = cv2.putText(result_image, "Total Objects: "+str(count-1), (200,50), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255), 1)
+
 cv2.imshow("Binary Image", binary)
+cv2.imshow('Final segmented Image', result_image)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-img = cv2.connectedComponents(binary)
-print("Total Objects: ",img[0]-1)
+print("Total Objects: ",count-1)
